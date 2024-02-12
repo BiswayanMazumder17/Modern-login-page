152203bab5f6230db6f186f67b8b24f847f3f33a
Biswayan17 <damnbiswayan1701@gmail.com>
2024-02-13 00:30:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("login-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Replace with your login logic
+    // For demonstration purposes, just open a new tab
+    window.open("amazon.html", "_blank");
+  });
+  
